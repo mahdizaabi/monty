f33bc82ce8964fa6434a0ac4ba1dab435d2c234f
Username <mahdi.zaabi@gmail.com>
2020-05-16 16:33:10 +0000

    adding_file

--- display.c
+++ display.c
@@ -17,3 +17,21 @@ void pall_me(stack_t **stack, unsigned int ln)
 	free(temp);
 	temp = NULL;
 }
+
+/**
+  * pint - print from top of the stack
+  * @stack: pointer to stack
+  * @ln: number of lines
+  */
+void pint_me(stack_t **stack, unsigned int ln)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", ln);
+		clean_stack(*stack);
+		clean_all();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", (*stack)->n);
+}
